de6f764f5a9a251b65698c56aed43318a3cac98e
analuiza7 <107579995+analuiza7@users.noreply.github.com>
2024-05-29 15:49:53 -0300

    J

---
+++ script.js
@@ -0,0 +1,42 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunstas = document.querySelector("caixa-perguntas");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultados");
+const caixaResultados = document.querySelector("texto-resultados");
+
+const perguntas = [
+    {
+enunciado: "Pergunta 1",
+alternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]
+    },
+    { 
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+            },
+            {    enunciado: "Pergunta 3",
+            alternativas: [
+                "Alternativa 1",
+                "Alternativa 2"
+            ]
+                },
+                {    
+                    enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+            },
+            {    
+                enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+            },
+        ];   
