5fc60aaba2eac6e2b462018449e224d8d582cc12
analuiza7 <107579995+analuiza7@users.noreply.github.com>
2024-06-05 15:41:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,35 +9,34 @@ const perguntas = [
         enunciado: "No futuro do agro, a tecnologia revolucionará a agricultura, aumentando a produtividade e a sustentabilidad.O que você acha que será uma grande inovação para o setor?",
         alternativas: [
             "Uso de drones para monitoramento de lavouras!",
-            "Isso é maravilhoso!"
+            "Implementação de mais inteligência nos maquinários!"
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Qual tecnologia vocễ acha que tem maior potencial de transformar a agricultura no futuro ?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            "Uso de robos para monitoramento de lavarouras.",
+            "Máquinas agrícolas movidas a carvão.",
+     },
+    {
+        enunciado: "Como você acha que a inteliễncia artificial pode beneficiar o setor agrícula no futuro?",
+        alternativas: [
+            "Aumentando a eficiência na gestão de fazendas.",
+            "Reduzindo a necessidade de águas nas plantações."
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Quais são os possíveis impactos da nanotecnogia na agricultura do futuro?",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            "Melhoria da quantidade dos alimentos e aumento de eficiência na aplicação de fertilizantes.",
+            "Aumento da poluição do solo e contaminação dos alimentos ."
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Além da produção de alimento,o que mais o futuro do agro pode abranger?",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+           "Bioenergia e produtos farmacêuticos.",
+            "Apenas alimentos."
         ]
     },
 ];
