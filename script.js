9a492bf2a3675237ce088f6853136fb92a41b369
analuiza7 <107579995+analuiza7@users.noreply.github.com>
2024-06-12 14:41:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,35 +8,35 @@ const perguntas = [
     {
         enunciado: "No futuro do agro, a tecnologia revolucionará a agricultura, aumentando a produtividade e a sustentabilidad.O que você acha que será uma grande inovação para o setor?",
         alternativas: [
-            "Uso de drones para monitoramento de lavouras!",
-            "Implementação de mais inteligência nos maquinários!"
+           texto: "Uso de drones para monitoramento de lavouras!",
+            afirmacao: "Implementação de mais inteligência nos maquinários!"
         ]
     },
     {
         enunciado: "Qual tecnologia vocễ acha que tem maior potencial de transformar a agricultura no futuro ?",
         alternativas: [
-            "Uso de robos para monitoramento de lavarouras.",
-            "Máquinas agrícolas movidas a carvão.",
+           texto: "Uso de robos para monitoramento de lavarouras.",
+              afirmacao: "Máquinas agrícolas movidas a carvão.",
      },
     {
         enunciado: "Como você acha que a inteliễncia artificial pode beneficiar o setor agrícula no futuro?",
         alternativas: [
-            "Aumentando a eficiência na gestão de fazendas.",
-            "Reduzindo a necessidade de águas nas plantações."
+         texto:   "Aumentando a eficiência na gestão de fazendas.",
+            afirmacao:  "Reduzindo a necessidade de águas nas plantações."
         ]
     },
     {
         enunciado: "Quais são os possíveis impactos da nanotecnogia na agricultura do futuro?",
         alternativas: [
-            "Melhoria da quantidade dos alimentos e aumento de eficiência na aplicação de fertilizantes.",
-            "Aumento da poluição do solo e contaminação dos alimentos ."
+          texto:  "Melhoria da quantidade dos alimentos e aumento de eficiência na aplicação de fertilizantes.",
+             afirmacao: "Aumento da poluição do solo e contaminação dos alimentos ."
         ]
     },
     {
         enunciado: "Além da produção de alimento,o que mais o futuro do agro pode abranger?",
         alternativas: [
-           "Bioenergia e produtos farmacêuticos.",
-            "Apenas alimentos."
+          texto: "Bioenergia e produtos farmacêuticos.",
+            afirmacao:  "Apenas alimentos."
         ]
     },
 ];
